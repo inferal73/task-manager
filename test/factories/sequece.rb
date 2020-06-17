dc5615fc2805a07d849b60c650bdcc1b90d85dce
Ivan <inferal73@gmail.com>
2020-06-17 14:52:51 +0400

    adds user and task model

---
+++ test/factories/sequece.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  sequence :string, aliases: [:first_name, :last_name, :name, :description, :state, :password, :avatar] do |n|
+    "string#{n}"
+  end
+
+  sequence :expired_at do |n|
+    today = Date.today
+    today + 3
+  end
+
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+end
