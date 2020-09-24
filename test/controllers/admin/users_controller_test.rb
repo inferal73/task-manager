eec70ec9e9f1b619832d0679e2dbfd084a7dbaa0
Ivan <inferal73@gmail.com>
2020-09-24 16:27:23 +0400

    adds create & edit user

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -16,4 +16,35 @@ class Admin::UsersControllerTest < ActionController::TestCase
     get :show, params: { id: user.id }
     assert_response :success
   end
+
+  test "should get edit" do
+    user = create(:user)
+    get :edit, params: { id: user.id }
+    assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should post create" do
+    user = attributes_for(:user)
+    assert_difference 'User.count' do
+      post :create, params: { user: user }
+    end
+    assert_response :redirect
+  end
+ 
+  test "should patch update" do
+    user = create(:user)
+    user_attrs = attributes_for(:user)
+    patch :update, params: { id: user.id, user: user_attrs }
+    patched_user = User.find(user.id)
+
+    assert patched_user[:first_name] == user_attrs[:first_name]
+    assert patched_user[:last_name] == user_attrs[:last_name]
+    assert patched_user[:email] == user_attrs[:email]
+    assert patched_user.authenticate(user_attrs[:password]) == patched_user
+  end
 end
